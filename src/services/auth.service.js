d1de6f1b6b6ed7d67a063caae2e38e0032db8b5c
unknown <similan.klinsmith@mail.kmutt.ac.th>
2021-11-20 16:06:55 +0700

    :art: [UPDATE] api path

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 // Authentication service
 import axios from "axios";
-const API_URL = "http://localhost:9000/api/";
+const API_URL = "https://www.clothshop.company/api";
 
 class AuthService {
   login(user) {
